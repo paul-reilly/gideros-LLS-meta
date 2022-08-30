477c770ff8c3480f251b369385100a43f7341aa4
Paul Reilly <paul-reilly@users.noreply.github.com>
2022-08-30 23:06:05 +0100

    updates core

--- library/gideros.lua
+++ library/gideros.lua
@@ -7,8 +7,19 @@
 ---
 ---@class Object
 Object = {}
+
+---
+---Called by `.new` function of inheriting objects
+---To be overriden in inheriting class, if required.
+---
+---@param ... any # parameters defined in inheriting class
 function Object:init(...) end
 
+---
+---Called after `.new` function of inheriting class calls `:init`
+---To be overriden in inheriting class, if required.
+---
+---@param ... any # parameters defined in inheriting class
 function Object:postInit(...) end
 
 ---
@@ -37,7 +48,7 @@ function Object:isInstanceOf(classname) end
 Core = {}
 
 ---
----Creates and returns new Gideros class. 
+---Creates and returns new Gideros class type. 
 ---
 ---@generic T
 ---@param type T Object Gideros class from which to inherit
@@ -45,6 +56,29 @@ Core = {}
 ---@param destructor? function Function called when the object is garbage collected 
 ---@return T object
 function Core.class(type, constructor, destructor) end
+
+---
+---Launches a function on a separate thread as a background task.
+---Background threads are only executed when the main thread is not running. 
+---@param task function # function to be run in background
+---@param ...? any # parameter(s) to pass to function
+function Core.asyncCall(task, ...) end
+
+---
+---Generates and returns a pseudo random number. The random number sequence is guaranteed to be consistent across devices for a given seed.
+---If bounds aren't specified, the function will return a floating point number between 0 and 1. If one bound is specified, the function will return an integer number between 1 and that bound (inclusive). If both bounds are specified, the function will return an integer between bound1 and bound2. bound2 must be greater than bound1. 
+--- 
+---@param generator? number # PRNG algorithm to use, use 0 for default engine (MT19937)
+---@param bound1? number # first bound of the range returned number must be in 
+---@param bound2? number # second bound of the range returned number must be in 
+---@return number|integer random_number # see description of function
+function Core.random(generator, bound1, bound2) end
+
+---
+---Set the current seed of the specified random number generator. 
+---@param generator? number # PRNG algorithm to use, use 0 for default engine (MT19937)
+---@param seed? number # seed for that generator
+function Core.randomSeed(generator, seed) end
 
 ---@class vector
 ---@field x number
